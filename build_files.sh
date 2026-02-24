6a3e80a46987795c2e1be44e6a3248b10a786817
Shahanwaj Khan <149888375+shahanwajkhan@users.noreply.github.com>
2026-02-24 12:29:53 +0530

    Add Vercel deployment configuration

---
+++ build_files.sh
@@ -0,0 +1,5 @@
+echo "Building project..."
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput
+echo "Build complete."
--- ums_project/settings.py
+++ ums_project/settings.py
@@ -31,8 +31,7 @@ CLOUDFLARE_SITE_KEY = os.getenv('CLOUDFLARE_SITE_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 # Application definition
 
@@ -125,8 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Media files
